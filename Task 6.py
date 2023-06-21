401570f599500a0b523efa970928a6660364d389
Yana Boldareva <136881982+Yana-Bo@users.noreply.github.com>
2023-06-21 14:42:10 +0300

    Add files via upload

---
+++ Task 6.py
@@ -0,0 +1,4 @@
+def sum_of_num(a, b):
+    return a + b
+print(sum_of_num(4, 1))
+print(sum_of_num(9, 5))
---
+++ Task 7.py
@@ -0,0 +1,5 @@
+def sum_of_num(a, b):
+    return a + b
+n1 = int(input("Введите любое число - "))
+n2 = int(input("Введите любое число - "))
+print(sum_of_num(n1, n2))
