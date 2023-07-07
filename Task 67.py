5e7e87a8344b4c2bacd8ed293ce63fc0e54c60e5
Yana Boldareva <136881982+Yana-Bo@users.noreply.github.com>
2023-07-07 22:17:33 +0300

    Add files via upload

--- Task 67.py
+++ Task 67.py
@@ -1,25 +1,24 @@
 import sqlite3
 
-conn = sqlite3.connect('staff.db')
+conn = sqlite3.connect('books.db')
 cursor = conn.cursor()
-cursor.execute('''CREATE TABLE IF NOT EXISTS staff
-(id INTEGER PRIMARY KEY, name TEXT, position INTEGER, zp INTEGER)''')
+cursor.execute('''CREATE TABLE IF NOT EXISTS books
+(id INTEGER PRIMARY KEY, name TEXT, author TEXT, year INTEGER)''')
 
-staff = [('Вика', 'эксперт', 45000),
-         ('Андрей', 'бухгалтер', 54000),
-         ('Олег', 'менеджер', 34000),
-         ('Валентина', 'экономист', 48500),
-         ('Дарья', 'руководитель', 85000),
-         ('Михаил', 'менеджер', 35000),
-         ('Оксана', 'специалист', 45000),
-         ('Алена', 'менеджер', 35000)]
+books = [('Мемуары Гейши', 'Артур Голден', 1997),
+         ('Цвет волшебства', 'Терри Пратчетт', 1983),
+         ('Американские боги', 'Нил Гейман', 2001),
+         ('Мы', 'Евгений Замятин', 1924),
+         ('1984', 'Джордж Оруэлл', 1949),
+         ('Есть, молиться, любить', 'Элизабет Гилберт', 2006),
+         ('Понедельник начинается в субботу', 'братья Стругацкие', 1965),
+         ('Благие знамения', 'Терри Пратчетт', 1990)]
 
-cursor.executemany("INSERT INTO staff (name, position, zp) VALUES (?, ?, ?)", staff)
+cursor.executemany("INSERT INTO books (name, author, year) VALUES (?, ?, ?)", books)
 conn.commit()
-cursor.execute('SELECT * FROM staff WHERE position = "менеджер"')
+cursor.execute('SELECT * FROM books WHERE year > 2000')
 rows = cursor.fetchall()
 
 for row in rows:
-    print(f'Имя - {row[1]}, должность - {row[2]}, зарплата {row[3]} р.')
-
+    print(f'Книга - {row[1]}, автор {row[2]}, год издания - {row[3]}')
 conn.close()
